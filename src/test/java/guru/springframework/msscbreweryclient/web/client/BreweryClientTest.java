13f670bef3862921a260092b7beb4d30024d4b09
ledio.papa@fti.edu.al <n25I^kW4bfhW>
2020-05-03 15:01:20 +0200

    created client object host path added as a property created tests for getBeerById and saveNewBeer

---
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,38 @@
+package guru.springframework.msscbreweryclient.web.client;
+
+import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Created by ledio on 5/3/20
+ */
+@SpringBootTest
+class BreweryClientTest {
+
+  @Autowired
+  BreweryClient client;
+
+  @Test
+  void getBeerById() {
+    BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+    assertNotNull(beerDto);
+  }
+
+  @Test
+  void saveNewBeer() {
+    //given
+    BeerDto beerDto = BeerDto.builder().beerName("New Beer").build();
+
+    URI uri = client.saveNewBeer(beerDto);
+
+    assertNotNull(uri);
+    System.out.println(uri.toString());
+  }
+}
