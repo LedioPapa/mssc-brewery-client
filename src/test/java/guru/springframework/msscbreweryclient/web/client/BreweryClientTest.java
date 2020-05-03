60381dca55e521b0a9d8cf9e85bf7209149079c9
ledio.papa@fti.edu.al <n25I^kW4bfhW>
2020-05-03 15:52:06 +0200

    created get, post, put and delete api calls for customer added tests

--- src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
+++ src/test/java/guru/springframework/msscbreweryclient/web/client/BreweryClientTest.java
@@ -1,6 +1,7 @@
 package guru.springframework.msscbreweryclient.web.client;
 
 import guru.springframework.msscbreweryclient.web.model.BeerDto;
+import guru.springframework.msscbreweryclient.web.model.CustomerDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -43,5 +44,41 @@ class BreweryClientTest {
     UUID uuid = UUID.randomUUID();
 
     client.updateBeer(uuid, beerDto);
+  }
+
+  @Test
+  void deleteBeer() {
+    client.deleteBeer(UUID.randomUUID());
+  }
+
+  @Test
+  void getCustomerById() {
+    CustomerDto customerDto = client.getCustomerById(UUID.randomUUID());
+    assertNotNull(customerDto);
+  }
+
+  @Test
+  void saveNewCustomer() {
+    //given
+    CustomerDto customerDto = CustomerDto.builder().name("New Customer").build();
+
+    URI uri = client.saveNewCustomer(customerDto);
+
+    assertNotNull(uri);
+    System.out.println(uri.toString());
+  }
+
+  @Test
+  void updateCustomer() {
+    //given
+    CustomerDto customerDto = CustomerDto.builder().name("Existing Customer").build();
+    UUID uuid = UUID.randomUUID();
+
+    client.updateCustomer(uuid, customerDto);
+  }
+
+  @Test
+  void deleteCustomer() {
+    client.deleteCustomer(UUID.randomUUID());
   }
 }
